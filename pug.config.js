8d100e1c35810e97772bd077a4127142697b1887
Derek Christensen <covfefe24@protonmail.com>
2019-04-11 11:47:03 -0400

    First commit before turn-in

---
+++ pug.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    locals: {
+        greeting: "Hello",
+        name: "Derek Christensen",
+        title: "Derek Christensen's Portfolio",
+        navLinks: ['home', 'about', 'resume'],
+        homePage: {
+            greeting: "Hello front page!",
+        },
+        contactPage: {
+
+        },
+        bioPage: {
+
+        }
+    }
+}
